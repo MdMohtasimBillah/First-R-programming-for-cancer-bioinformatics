c110d2144acac305235652dc7510800d48ac4a99
Md Mohtasim Billah <rifatmohtasim@gmail.com>
2024-02-28 21:22:52 +0600

    First R script for upload

---
+++ Script/ learning.R
@@ -0,0 +1,39 @@
+str <- "CHIRAL Bd"
+class(str)
+
+Message_1 <- "This is Rifat."
+Message_2 <- "He is student"
+Out <- paste(Message_1,Message_2)
+out
+Out
+Lower <- tolower(Out)
+Lower
+
+Upper <- toupper(Out)
+Upper
+
+
+Vector <- c(1,2,3,4,5,66,52,12,85)
+Vector
+class(Vector)
+
+vector_1 <- C(1:32)
+vector_1
+
+ seq(1,32, 3)
+
+vector_2 <- c(T,F,F,F,T,21,"Zim")
+class(vector_2)
+vector_2[7]
+vector_2[c(2:7)]
+
+for_index <- c(12,52,63,58)
+for_index[3]
+
+colors <- c("red", "green","blue","golden", "orange", "black","silver" )
+colors[c(2:5)]
+colors[c(3,5,1,7)]
+class(colors)
+
+install.packages("usethis")
+
