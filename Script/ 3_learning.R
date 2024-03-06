dc29790c36d46d19c54b5cfccb8080d50bc66c60
Md Mohtasim Billah <rifatmohtasim@gmail.com>
2024-03-07 01:21:57 +0600

    Loop learning

---
+++ Script/ 3_learning.R
@@ -0,0 +1,60 @@
+# if statement
+num <- -10
+if(num < 0){
+  print("positive")
+}
+
+# if...else function
+if(num > 0){
+  print("positive")
+} else{"negative"}
+
+#if...else if...else function
+4 == 2 & 5 == 2
+4>5 | 0==0
+BMI <- 21
+if(BMI >= 18 & BMI < 25){
+  print("normal")
+} else if(BMI >= 25 & BMI < 30){
+  print("Overweight")
+} else ("obese")
+
+# All the above code will properly run if the variable contain one data or objective and for numeric
+
+#ifelse function
+ages <- c(12,23,45)
+ifelse(ages>18, "adult","child")
+
+# for loop
+for(variable in sequence){
+  # do something
+}
+
+for(i in 1:9){
+  print("Go for Jihad")
+}
+
+for(a in 1:10){print(a)}
+for(b in 10:1){print(b)}
+colors <- c("black", "blue", "gold", "pink")
+for(d in colors){print(d)}
+
+# next is a statement that used for ignore or prevent exact value
+for(i in 1:100){
+  if(i == 5)
+  {next}
+    print(i)
+}
+
+"v"
+bb <- "rifat"
+bb
+
+# break is a statement that used to stop printing the sequence
+for(i in 1:100){
+  if(i == 5)
+  {break}
+  print(i)
+}
+
+# while loop
