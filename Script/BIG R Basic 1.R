1873d401e6a44164973d2e6b8baf82faf715e543
MdMohtasimBillah <rifatmohtasim@gmail.com>
2025-12-05 22:00:13 +0600

    12

---
+++ Script/BIG R Basic 1.R
@@ -0,0 +1,43 @@
+# Basic Data Syntax
+
+CHIRAL Bangladesh
+
+"CHIRAL Bangladesh"
+
+1212
+
+"1212mnm"
+
+
+# Variables
+
+V1 <- "CHIRAL Bangladesh"
+V2 <- 1212
+V3 <- "CHIRAL Bangladesh", 1212
+
+
+typeof(V1)
+typeof(V2)
+
+
+# Data Types (Vector)
+
+c("CHIRAL Bangladesh", 1212, "1212")
+
+V4 <- c("CHIRAL Bangladesh", 1212, "1212")
+V5 <- c(1,2,3,6,5,8)
+
+typeof(V4)
+typeof(V5)
+
+
+
+
+
+
+
+
+
+
+
+
