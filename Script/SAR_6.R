e360d8ad8b2f56aa9d0888671c594cb556b5c25b
MdMohtasimBillah <rifatmohtasim@gmail.com>
2025-12-20 21:24:26 +0600

    20.12.2025

--- Script/SAR_6.R
+++ Script/SAR_6.R
@@ -53,9 +53,41 @@ data |>
 
 
 
+# read the data
+data <- read.csv("data/Thalassemia_QoL_Processed.csv")
 
 
 
+# Table 1 Demonstrates the demographic characteristics of the patients (N = 365)
+data |>
+  select(1:8) |>
+  tbl_summary() |>
+  as_gt() |>
+  gtsave("qol_tables/Table1.docx")
+
+# Table 2 Clinical characteristics of thalassemia patients (N = 356).
+data |>
+  select(9:17) |>
+  tbl_summary(
+    type = c(Severity, Previous_3Months_Transfution) ~ "categorical"
+  ) |>
+  as_gt() |>
+  gtsave("qol_tables/Table2.docx")
+
+
+
+
+# Table 3 Health-related Quality of life (QoL) of thalassemia patients, according to Short Form (SF-36) score.
+data |>
+  select(18:28, Gender) |>
+  tbl_summary(
+    type = everything() ~ "continuous",
+    by = Gender
+  ) |>
+  add_p() |>
+  bold_p(t = 0.05) |>
+  as_gt() |>
+  gtsave("qol_tables/Table3.docx")
 
 
 
